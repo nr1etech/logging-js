811829f78177f7cf5be2e5e178d0773441b38c58
Erik Jensen <365211+erikrj@users.noreply.github.com>
2023-12-30 12:26:12 -0700

    Fixing test

--- logging.test.ts
+++ logging.test.ts
@@ -1,18 +1,12 @@
-import {getLogger, getRootLogger} from './logging';
+import * as logging from './logging';
 
 test('Test Logging', () => {
-  const root = getRootLogger('logging.test.ts');
-  root.trace({foo: 'bar', oink: 'moo'}, 'hello world1');
-  root.debug({foo: 'bar'}, 'hello world2');
-  root.info({foo: 'bar'}, 'hello world3');
-  root.warn({foo: 'bar'}, 'hello world4');
-  root.error({foo: 'bar'}, 'hello world5');
-  root.fatal({foo: 'bar'}, 'hello world6');
-  const child = getLogger('child');
-  child.trace({foo: 'bar', oink: 'moo'}, 'hello world7');
-  child.debug({foo: 'bar'}, 'hello world8');
-  child.info({foo: 'bar'}, 'hello world9');
-  child.warn({foo: 'bar'}, 'hello world10');
-  child.error({foo: 'bar'}, 'hello world11');
-  child.fatal({foo: 'bar'}, 'hello world12');
+  logging.initialize('logging-js');
+  const child = logging.getLogger('child');
+  child.trace({foo: 'bar', something: 'something'}, 'test trace');
+  child.debug({foo: 'bar'}, 'test debug');
+  child.info({foo: 'bar'}, 'test info');
+  child.warn({foo: 'bar'}, 'test warn');
+  child.error({foo: 'bar'}, 'test error');
+  child.fatal({foo: 'bar'}, 'test fatal');
 });
