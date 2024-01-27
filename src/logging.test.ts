8ffacda66b6b14f817de04b325a69437c389b70c
Erik Jensen <365211+erikrj@users.noreply.github.com>
2024-01-27 06:46:39 -0700

    Adding test

--- src/logging.test.ts
+++ src/logging.test.ts
@@ -5,6 +5,8 @@ test('Test Logging', () => {
     level: 'trace',
     svc: 'logging-js',
   });
+  const root = logging.getRootLogger();
+  expect(root).toBeDefined();
   const child = logging.getLogger('child');
   child.trace('test trace');
   child.trace({foo: 'bar', something: 'something'}, 'test trace');
@@ -20,4 +22,14 @@ test('Test Logging', () => {
   }
   child.info('This completes our %s', 'test');
   child.fatal({foo: 'bar'}, 'test fatal');
+});
+
+test('Test isLevel', () => {
+  expect(logging.isLevel('trace')).toBe(true);
+  expect(logging.isLevel('debug')).toBe(true);
+  expect(logging.isLevel('info')).toBe(true);
+  expect(logging.isLevel('warn')).toBe(true);
+  expect(logging.isLevel('error')).toBe(true);
+  expect(logging.isLevel('fatal')).toBe(true);
+  expect(logging.isLevel('foo')).toBe(false);
 });
