c115d05843efe8bfac1aa25257a6e3e48f652d7a
Erik Jensen <365211+erikrj@users.noreply.github.com>
2024-08-03 08:52:11 -0600

    Fixing eslint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,9 +3,13 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
-const config = tseslint.config(
-  eslint.configs.recommended,
-  ...tseslint.configs.recommended,
-);
-config[0].ignores = ['coverage/**', 'dist/**', 'site/**'];
+const config = [
+  {
+    ignores: ['src/**/*.mjs', 'src/**/*.d.*'],
+  },
+  ...tseslint.config(
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
+  ),
+];
 export default config;
